26a07941f62067eb58824a9dedfec2a4d2a9f2aa
Thomas Guillem <thomas@gllm.fr>
2025-06-09 08:29:06 +0200

    FileSelector: fix typo in case of DEM lookup error

--- ui/file_selector.py
+++ ui/file_selector.py
@@ -212,7 +212,7 @@ class FileSelector(QMainWindow):
         self.join_thread(False)
 
         if fit_file.elevation_error_rate != 0:
-            elevation_error_rate = int(file_file.elevation_error_rate * 100)
+            elevation_error_rate = int(fit_file.elevation_error_rate * 100)
             QMessageBox.warning(self, "Invalid DEM File",
                                 f"Could not correct {elevation_error_rate}% of altitude points")
 
